2023e6df15f6ffd7cc2edf67f9d228dfd5797518
jedhabush <jedhabush@gmail.com>
2022-12-28 13:00:02 +1100

    phase two => populating videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  // selected category on the sidebar
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  // videos state
+  const [videos, setVideos] = useState([]);
+
+  // fetching the data
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +25,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -21,10 +37,17 @@ const Feed = () => {
           Copyright 2023 Jed Habush
         </Typography>
       </Box>
-      <Box>
-        <Typography>
-          New <span style={{ color: "#FC1503" }}>Videos</span>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
