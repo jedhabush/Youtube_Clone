b7d9172b743ee1f3c2c77a9a55fa04a593c506fd
jedhabush <jedhabush@gmail.com>
2022-12-28 13:17:47 +1100

    checks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 import SearchBar from "./SearchBar";
-
+//navbar tesst
 const Navbar = () => (
   <Stack
     direction="row"
